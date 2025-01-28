7b94cae04f8085121f2cfedd9bdab2bef16ac353
cemdemircii <demircicem1907@gmail.com>
2025-01-28 23:39:53 +0300

    Add project files.

---
+++ MyRapidApiProject/Controllers/ExchangeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace MyRapidApiProject.Controllers
+{
+	public class ExchangeController : Controller
+	{
+		public async Task< IActionResult> Index()
+		{
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=EUR&amount=750"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "68eb318446mshffd79b1cafbc963p11cf5djsncafe11ee2e5d" },
+		{ "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+			    var json=JObject.Parse(body);
+				ViewBag.v = json["result"];
+			}
+			return View();
+		}
+	}
+}
---
+++ MyRapidApiProject/Controllers/ImdbMovieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRapidApiProject.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MyRapidApiProject.Controllers
+{
+    public class ImdbMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "68eb318446mshffd79b1cafbc963p11cf5djsncafe11ee2e5d" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject < List<ImdbMovieViewModel>> (body);
+                return View(values.ToList());
+            }
+
+        }
+    }
+}
